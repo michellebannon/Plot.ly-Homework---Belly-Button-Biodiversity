ffd841ee257fd5e4a173c31e8a21fbe050d34eda
Michelle <michellerbannon@gmail.com>
2021-01-18 16:00:06 -0600

    worked with tutor to create bar and bubble chart

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,7 @@
-
 d3.json('samples.json').then(data => {
-    var { names } = data;
+    var {
+        names
+    } = data;
     names.forEach(name => {
         d3.select('select').append('option').text(name);
     });
@@ -9,42 +10,49 @@ d3.json('samples.json').then(data => {
 showData();
 
 function showData() {
-    
-    // Use the D3 library to read in `samples.json`
     d3.json('samples.json').then(data => {
-        var { metadata, samples} = data;
-        // select the values of the metadata;worked with tutor to develop this
+        var { metadata, samples } = data;
+
         var selection = d3.select('select').property('value');
         metadata = metadata.filter(obj => obj.id == selection)[0]
         samples = samples.filter(obj => obj.id == selection)[0]
         // created the select in the demographic area; worked with the tutor to develop this
         d3.select('.panel-body').html('');
-        Object.entries(metadata).forEach(([key,val])=> {
+        Object.entries(metadata).forEach(([key, val]) => {
             d3.select('.panel-body').append('h5').text(`${key}: ${val}`)
         });
-    
-    //get top 10 OTU ID's for plot
-        var OTU_top = (sampledate.samples[0].otu_ids.slice(0, 10)).reverse();
+
+        console.log(metadata.wfreq);
+        var { otu_ids, sample_values, otu_labels } = samples;
+
+        var barData = [{
+            x: sample_values.slice(0, 10).reverse(),
+            y: otu_ids.slice(0, 10).reverse().map(val => `OTU ${val}`),
+            text: otu_labels.slice(0, 10).reverse(),
+            marker: {
+                color: 'blue'
+            },
+            type: "bar",
+            orientation: "h",
+        }];
+
+        Plotly.newPlot('bar',barData)
+
+        var bubbleData = [{
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: "markers",
+            marker: {
+                size: sample_values,
+                color: otu_ids
+            }
+        }];
+
+        Plotly.newPlot('bubble',bubbleData);
+
        
-    //get the OTU ide's for the plot
-        var OTU_id = OTU_top.map(d => "OTU " + d);
-        console.log('OTU IDS: ${OTU_id}')
-    
-    // get the top 10 labels for the plot
-    var labels =  sampledata.samples[0].otu_labels.slice(0,10);
-    console.log(`OTU_labels: ${labels}`)
-    var trace = {
-        x: sampleValues,
-        y: OTU_id,
-        text: labels,
-        marker: {
-        color: 'blue'},
-        type:"bar",
-        orientation: "h",
-    };
-// create data variable
-var data = [trace];
-        // console.log(metadata);
+
     });
 };
 
